9d05c7ebb783ffbc1eb0526cd1611f17f432b599
RuggeDX <rishwan@live.nl>
2021-01-22 20:45:18 +0100

    Added different files for different tests

---
+++ WDPR_MVC/WDPR_MVC_TESTS/BaseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WDPR_MVC.Areas.Identity.Data;
+using WDPR_MVC.Data;
+using WDPR_MVC.Models;
+
+namespace WDPR_MVC_TESTS
+{
+    public class BaseTest
+    {
+        private string databaseName; // zonder deze property kun je geen clean context maken.
+
+        public MyContext GetInMemoryDBMetData()
+        {
+            MyContext context = GetNewInMemoryDatabase(true);
+
+            //Replace with other info
+            context.Add(new ApplicationUser { UserName = "Jan", Email = "test1@email.com", Adres = new Adres { Straatnaam = "De Straat", Huisnummer = 243, Postcode = "1237TK" } });
+            context.Add(new ApplicationUser { UserName = "Smit", Email = "test2@email.com", Adres = new Adres { Straatnaam = "De Weg", Huisnummer = 432, Postcode = "9342YO" } });
+            context.Add(new ApplicationUser { UserName = "Bruh", Email = "test3@email.com", Adres = new Adres { Straatnaam = "De Laan", Huisnummer = 12, Postcode = "4329FY" } });
+
+            context.SaveChanges();
+            return GetNewInMemoryDatabase(false); // gebruik een nieuw (clean) object voor de context
+        }
+
+        private MyContext GetNewInMemoryDatabase(bool NewDb)
+        {
+            if (NewDb) this.databaseName = Guid.NewGuid().ToString(); // unieke naam
+
+            var options = new DbContextOptionsBuilder<MyContext>()
+                .UseInMemoryDatabase(this.databaseName)
+                .Options;
+
+            return new MyContext(options);
+        }
+    }
+}
---
+++ WDPR_MVC/WDPR_MVC_TESTS/MeldingTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Text;
+using WDPR_MVC.Data;
+using System.Linq;
+
+namespace WDPR_MVC_TESTS
+{
+    public class MeldingTest : BaseTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var FoundUser = GetInMemoryDBMetData().Users.Any(u => u.Email == "test1@email.com");
+            Assert.True(FoundUser);
+        }
+    }
+}
