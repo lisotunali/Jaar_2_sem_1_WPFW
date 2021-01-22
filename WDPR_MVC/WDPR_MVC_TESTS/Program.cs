0e8c1942c29279a89dd1c96928d749a9f91c1f22
Brian Spit <brian@mythenatic.com>
2021-01-22 19:14:00 +0100

    Add test project

---
+++ WDPR_MVC/WDPR_MVC_TESTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WDPR_MVC_TESTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
